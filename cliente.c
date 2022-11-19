797014afade42d4481178054b4fc3d242e97ae88
Pichi <carrizoaagustin@gmail.com>
2022-11-18 21:56:56 -0300

    complete

--- cliente.c
+++ cliente.c
@@ -3,34 +3,103 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
-int main(void)
+#include <pthread.h>
+
+#define cantIp 254
+char enteroACaracter(int numero);
+void conexion(char *arg);
+void *prueba(char *arg);
+int main()
 {
-    int fd1; //Descriptores de archivo
-    int tama_sin;
-    struct sockaddr_in servidor; // Info de la direccion del cliente
-    char direccion [100];
-    printf("Ingrese su direccion de red");
-    scanf("%s",direccion);
+    char direccion[15];
+    pthread_t threads[cantIp];
+    printf("Ingrese su direccion de red: ");
+    scanf("%s", direccion);
     system("clear");
-    printf("\t********BIENVENIDO AL CLIENTE!********\n\n");
-    if ((fd1 = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+
+    int flag = 0;
+    int posicionHostID;
+
+    for (int i = 0; i < sizeof(direccion); i++)
+    {
+
+        if (flag > 2)
+        {
+            posicionHostID = i;
+            direccion[posicionHostID] = '\0';
+            break;
+        }
+
+        if (direccion[i] == '.')
+        {
+            flag++;
+        };
+    }
+
+    direccion[posicionHostID] = '0';
+    direccion[posicionHostID + 1] = '\0';
+
+    printf("Dirección de Red %s\n", direccion);
+    printf("Buscando servidores...\n");
+
+    for (int i = 0; i < cantIp; ++i)
+    {
+
+        if ((i + 1) < 10)
+        {
+            char c = enteroACaracter(i + 1);
+            direccion[posicionHostID] = c;
+        }
+        else if ((i + 1) < 100)
+        {
+            char c1 = enteroACaracter((i + 1) / 10);
+            char c2 = enteroACaracter((i + 1) % 10);
+            direccion[posicionHostID] = c1;
+            direccion[posicionHostID + 1] = c2;
+            direccion[posicionHostID + 2] = '\0';
+        }
+        else
+        {
+            char c1 = enteroACaracter((i + 1) / 100);
+            char c2 = enteroACaracter((i + 1) / 10 % 10);
+            char c3 = enteroACaracter((i + 1) % 10);
+            direccion[posicionHostID] = c1;
+            direccion[posicionHostID + 1] = c2;
+            direccion[posicionHostID + 2] = c3;
+            direccion[posicionHostID + 3] = '\0';
+        }
+
+        conexion(direccion);
+    }
+
+}
+
+char enteroACaracter(int numero)
+{
+    return numero + '0';
+}
+
+void conexion(char *dir)
+{
+    int Descriptor;
+    struct sockaddr_in servidor;
+    if ((Descriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("Error en Socket()\n");
         exit(-1);
     }
-    for (size_t i = 1; i <= 254; i++)
-    {
-        servidor.sin_family = AF_INET;
-        servidor.sin_port = htons(80); // Convierte PORT al numero de red
-        servidor.sin_addr.s_addr = inet_addr(strcat(direccion,strcat('.',char(i)));
-        bzero(&(servidor.sin_zero), 8); //Coloca ceros en resto estructura
+    servidor.sin_family = AF_INET;
+    servidor.sin_port = htons(80);
+    servidor.sin_addr.s_addr = inet_addr(dir);
+    bzero(&(servidor.sin_zero), 8);
 
-        if (connect(fd1, (struct sockaddr *)&servidor, sizeof(servidor)) != 0)
-        {
-            printf("connection with the server failed...\n");
-            exit(0);
-        }
-        else
-            printf("connected to the server..\n");
+    if (connect(Descriptor, (struct sockaddr *)&servidor, sizeof(servidor)) < 0)
+    {
+        printf("No se encontro Servidor en IP: %s\n", dir);
+    }
+    else
+    {
+        printf("Se encontro Servidor en IP: %s\n", dir);
+        close(Descriptor);
     }
 }
